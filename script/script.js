01bf2cebec105399dfbfd513057f61891f386c0f
Alessandro Montenero <alemonte99@icloud.com>
2023-02-21 20:12:43 +0100

    added instructors section

--- script/script.js
+++ script/script.js
@@ -1,76 +1,169 @@
 
-
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        navItems: [
-          {
-            label: 'HOME',
-            url: '#',
-            new: false
-          },
-          {
-            label: 'ABOUT',
-            url: '#',
-            new: false
-          },
-          {
-            label: 'PRICES',
-            url: '#',
-            new: false
-          },
-          {
-            label: 'COURSES',
-            url: '#',
-            new: true
-          },
-          {
-            label: 'LOCATIONS',
-            url: '#',
-            new: false
-          },
-          {
-            label: 'BLOG',
-            url: '#',
-            new: false
-          },
-        ]
-      }
+createApp({
+  data() {
+    return {
+      navItems: [
+        {
+          label: 'HOME',
+          url: '#',
+          new: false
+        },
+        {
+          label: 'ABOUT',
+          url: '#',
+          new: false
+        },
+        {
+          label: 'PRICES',
+          url: '#',
+          new: false
+        },
+        {
+          label: 'COURSES',
+          url: '#',
+          new: true
+        },
+        {
+          label: 'LOCATIONS',
+          url: '#',
+          new: false
+        },
+        {
+          label: 'BLOG',
+          url: '#',
+          new: false
+        },
+      ]
     }
-  }).mount('#navbar')
+  }
+}).mount('#navbar')
 
-  createApp({
-    data() {
-      return {
-        newName: '',
-        newEmail: '',
-        newTelephone: '',
-        newLocation: ''
+createApp({
+  data() {
+    return {
+      newName: '',
+      newEmail: '',
+      newTelephone: '',
+      newLocation: ''
+    }
+  },
+  methods: {
+    requestACallback() {
+      let newRequest = {
+        name: this.newName,
+        email: this.newEmail,
+        telephone: this.newTelephone,
+        location: this.newLocation
       }
-    },
-    methods: {
-      requestACallback() {
-        let newRequest = {
-          name: this.newName,
-          email: this.newEmail,
-          telephone: this.newTelephone,
-          location: this.newLocation
+      console.log(newRequest)
+      this.newName = ''
+      this.newEmail = ''
+      this.newTelephone = ''
+      this.newLocation = ''
+    }
+  }
+}).mount('#requestACallback')
+
+createApp({
+  data() {
+    return {
+      courses: [
+        {
+          label: 'Pass Plus',
+          img: 'courses-passplus.jpg'
+        },
+        {
+          label: 'Intensive Course',
+          img: 'course-intensive.jpg'
+        },
+        {
+          label: 'Instructors',
+          img: 'courses-instructor.jpg'
         }
-        console.log(newRequest)
-        this.newName = ''
-        this.newEmail = ''
-        this.newTelephone = ''
-        this.newLocation = ''
-      }
+      ]
     }
-  }).mount('#requestACallback')
+  }
+}).mount('#courses')
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
+createApp({
+  data() {
+    return {
+      animation: false,
+      ratesBoxes: [
+        {
+          label: 'PASS RATE',
+          value: '95',
+        },
+        {
+          label: 'REFERRAL RATE',
+          value: '100'
+        },
+        {
+          label: 'PASS RATE',
+          value: '0'
+        }
+      ],
+      contacts: [
+        {
+          name: 'Mike Hart',
+          img: 'instructor-mikehart.jpg',
+          descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, sunt!',
+          facebookLink: '#',
+          twitterLink: '#',
+          instagramLink: '#'
+        },
+        {
+          name: 'John Smith',
+          img: 'instructor-johnsmith.jpg',
+          descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, sunt!',
+          facebookLink: '#',
+          twitterLink: '#',
+          instagramLink: '#'
+        },
+        {
+          name: 'Angela Hart',
+          img: 'instructor-angelahart.jpg',
+          descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, sunt!',
+          facebookLink: '#',
+          twitterLink: '#',
+          instagramLink: '#'
+        }
+      ]
     }
-  }).mount('#footer')
+  },
+  methods: {
+    progressValue(index){
+      let deg = 0
+      let end = parseInt(this.ratesBoxes[index].value)
+      let speed = 6
+      let progress = setInterval(()=> {
+        if (deg != end) {
+          deg++;
+        }
+        if(deg >= end){
+          clearInterval(progress)
+        }
+        this.ratesBoxes[index].value = deg
+      }, speed)
+      
+    }
+  },
+  mounted () {
+    addEventListener("scroll", (event) => {  
+      let el = document.getElementById("coursesBottom")
+      var elDistanceToTop = parseInt((window.pageYOffset + el.getBoundingClientRect().top)) 
+      if (elDistanceToTop <= (window.pageYOffset + window.innerHeight)){
+          if (this.animation == false) {
+            this.progressValue(0)
+            this.progressValue(1)
+            this.progressValue(2)
+            this.animation = true
+          }
+      }
+    })
+    
+  }
+}).mount('#coursesBottom')
+
