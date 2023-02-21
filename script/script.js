61c2af6e67203591077852ba4eed21674df3b900
Alessandro Montenero <alemonte99@icloud.com>
2023-02-21 12:26:30 +0100

    first section

--- script/script.js
+++ script/script.js
@@ -1,4 +1,71 @@
+
+
 const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        navItems: [
+          {
+            label: 'HOME',
+            url: '#',
+            new: false
+          },
+          {
+            label: 'ABOUT',
+            url: '#',
+            new: false
+          },
+          {
+            label: 'PRICES',
+            url: '#',
+            new: false
+          },
+          {
+            label: 'COURSES',
+            url: '#',
+            new: false
+          },
+          {
+            label: 'LOCATIONS',
+            url: '#',
+            new: false
+          },
+          {
+            label: 'BLOG',
+            url: '#',
+            new: false
+          },
+        ]
+      }
+    }
+  }).mount('#navbar')
+
+  createApp({
+    data() {
+      return {
+        newName: '',
+        newEmail: '',
+        newTelephone: '',
+        newLocation: ''
+      }
+    },
+    methods: {
+      requestACallback() {
+        let newRequest = {
+          name: this.newName,
+          email: this.newEmail,
+          telephone: this.newTelephone,
+          location: this.newLocation
+        }
+        console.log(newRequest)
+        this.newName = ''
+        this.newEmail = ''
+        this.newTelephone = ''
+        this.newLocation = ''
+      }
+    }
+  }).mount('#requestACallback')
 
   createApp({
     data() {
@@ -6,4 +73,4 @@ const { createApp } = Vue
         message: 'Hello Vue!'
       }
     }
-  }).mount('#app')
+  }).mount('#footer')
